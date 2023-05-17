8aefd03ade4d9ce68db43dcc4dad17281b9edb9f
Chaahat <arorac2@mymacewan.ca>
2023-05-17 01:48:43 -0600

    updated code to work

---
+++ index2.js
@@ -0,0 +1,12 @@
+const { fetchMyIP } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
--- iss.js
+++ iss.js
@@ -22,57 +22,6 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-const fetchISSFlyOverTimes = function(coords, callback) {
-  const { latitude, longitude } = coords;
-  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      const errorMessage = `Request failed with status code ${response.statusCode}`;
-      callback(new Error(errorMessage), null);
-      return;
-    }
-
-    try {
-      const data = JSON.parse(body);
-      const flyOverTimes = data.response;
-      callback(null, flyOverTimes);
-    } catch (error) {
-      callback(error, null);
-    }
-  });
-};
-
-const nextISSTimesForMyLocation = function(callback) {
-  fetchMyIP((error, ip) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-
-    fetchCoordsByIP(ip, (error, coords) => {
-      if (error) {
-        callback(error, null);
-        return;
-      }
-
-      fetchISSFlyOverTimes(coords, (error, flyOverTimes) => {
-        if (error) {
-          callback(error, null);
-          return;
-        }
-
-        callback(null, flyOverTimes);
-      });
-    });
-  });
-};
-
 
 module.exports = { fetchISSFlyOverTimes,fetchCoordsByIP, nextISSTimesForMyLocation};
 
